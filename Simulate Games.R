ead0f7d5a440e485e96dba60f8ae42a3aaf7b0f1
Shahnur Islam <shahnurislam@hotmail.co.uk>
2017-10-25 23:29:07 +0100

    Add files via upload
    Correct Function

--- Simulate Games.R
+++ Simulate Games.R
@@ -10,34 +10,44 @@ Monty_Hall<-function(){
 }
 }
 
-Monty_Hall()
-#Add a reveal, that's why it isn't working!
+ifelse((Orig_Choice == prize), Wins <- Wins + 1, Loss <- Loss + 1)
+ifelse((New_Choice == prize), Wins <- Wins + 1, Loss <- Loss + 1)
 Play_Game <- function(n = 1,stick = TRUE){ #n is number of simulation and stick to your choice
-  Wins <<- 0
-  Loss <<- 0
+  Wins <- 0
+  Loss <- 0
   for (i in 1:n){
-      Orig_Choice <- sample(1:3,1)
-      
-      z <- c(1,2,3)
-      z <- z[z!=Orig_Choice]
-      New_Choice <- z[sample(1:2,1)]
-      Monty_Hall()
-      if(stick == TRUE){
-        ifelse((door[Orig_Choice] == "car"), Wins <- Wins +1, Loss <- Loss +1)
-      }else{
-        ifelse((door[New_Choice] == "car"), Wins <- Wins +1, Loss <- Loss +1)
-      }
-  }
-  return(paste("Wins:", Wins))
-  }
- 
-Play_Game(n = 10, stick = FALSE)
+    Orig_Choice <- sample(1:3,1) #Select a door
+    z <- 1:3
+    prize <-sample(1:3,1) #hide the car behind the door
+    
+    if(Orig_Choice != prize){
+      reveal <-(z[-c(Orig_Choice,prize)]) #Reveal a door with no prize
+    }else{
+      reveal <-sample(z[-c(prize,Orig_Choice)],1)
+    }
+    
+    # Deciding if we stick with our door or switch
+    if(stick == TRUE){
+      select <- Orig_Choice
+    }else{
+      select <- z[-c(reveal,Orig_Choice)]
+    }
+    
+    if(select == prize){
+      Wins <- Wins +1
+    }else{
+      Loss <- Loss +1
+    }
 
-Orig_Choice <- sample(1:3,1)
-z <- c(1,2,3)
-z <- z[z!=Orig_Choice]
-New_Choice <- z[sample(1:2,1)]
-Orig_Choice
-New_Choice
+    
+  }
+  cat(paste("Win: ", Wins/n*100,'%\nLoss:',Loss/n*100,'%\n'),sep="")
 
-ifelse((door[New_Choice] == "car"), print(1), Loss <- Loss +1)
+}
+
+plot(1:10);for(i in 1:10){points(10-i,i);Sys.sleep(1)}
+
+
+Play_Game(n = 100,stick = TRUE)
+
+z[-c(1,1)]
